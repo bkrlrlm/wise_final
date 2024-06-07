a591e4c76bfc8f28625a6f7692c803167c050e8d
bkrlrlm <131678391+bkrlrlm@users.noreply.github.com>
2024-06-07 19:31:52 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,7 @@
 
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 import pandas as pd
 
@@ -13,8 +14,9 @@ rf_classifier = joblib.load('random_forest_model.pkl')
 preprocessor = joblib.load('preprocessor.pkl')
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET'])
 def predict():
     data = request.json
     new_data_df = pd.DataFrame([data])
@@ -28,7 +30,7 @@ def predict():
     new_data_scaled = preprocessor.transform(new_data_df)
     
     prediction = rf_classifier.predict(new_data_scaled)
-    return jsonify({'predicted_job_role': prediction[0]})
+    return jsonify({'predicted_job_role': prediction[0]},message='CORS enabled')
 
 if __name__ == '__main__':
     app.run(debug=True)
