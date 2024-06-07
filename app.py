97a5d1c54e331eb26c12ee60de82e83ef46df5dd
bkrlrlm <131678391+bkrlrlm@users.noreply.github.com>
2024-06-07 21:38:18 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,37 +1,49 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 import pandas as pd
+import logging
 
-# 加載模型和預處理器
-rf_classifier = joblib.load('random_forest_model.pkl')
-preprocessor = joblib.load('preprocessor.pkl')
+
+logging.basicConfig(level=logging.INFO)
+
+try:
+    rf_classifier = joblib.load('random_forest_model.pkl')
+    preprocessor = joblib.load('preprocessor.pkl')
+except Exception as e:
+    logging.error(f"Error loading model or preprocessor: {e}")
+    raise e
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json
-    new_data_df = pd.DataFrame([data])
-    new_data_df = pd.get_dummies(new_data_df, columns=['Hard/Smart worker'], drop_first=True)
-    
-    for col in preprocessor.feature_names_in_:
-        if col not in new_data_df.columns:
-            new_data_df[col] = 0
-    
-    new_data_df = new_data_df[preprocessor.feature_names_in_]
-    new_data_scaled = preprocessor.transform(new_data_df)
-    
-    prediction = rf_classifier.predict(new_data_scaled)
-    return jsonify({'predicted_job_role': prediction[0]})
+    try:
+        data = request.get_json()  
+        logging.info(f"Received data: {data}")
+
+     
+        if 'Hard/Smart worker' not in data:
+            return jsonify({'error': "'Hard/Smart worker' field is missing"}), 400
+
+        new_data_df = pd.DataFrame([data])
+        new_data_df = pd.get_dummies(new_data_df, columns=['Hard/Smart worker'], drop_first=True)
+        
+        for col in preprocessor.feature_names_in_:
+            if col not in new_data_df.columns:
+                new_data_df[col] = 0
+        
+        new_data_df = new_data_df[preprocessor.feature_names_in_]
+        new_data_scaled = preprocessor.transform(new_data_df)
+        
+        prediction = rf_classifier.predict(new_data_scaled)
+        logging.info(f"Prediction: {prediction[0]}")
+
+        return jsonify({'predicted_job_role': prediction[0]})
+    except Exception as e:
+        logging.error(f"Error during prediction: {e}")
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
-
